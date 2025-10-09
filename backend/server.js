0c6323ff74f7f439d08667c61a14f053505615f1
moaaz malek <madhalfrwat@gmail.com>
2025-10-10 00:16:40 +0300

    Add vercel configuration

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,11 @@ connectDB();
 connectCloudinary();
 
 //middlewares
-app.use(cors());
+app.use(cors({
+    origin:process.env.CLIENT_URL,
+    credentials:true,
+    methods:["GET","POST","PUT","DELETE"]
+}));
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/admin", adminRoutes);
